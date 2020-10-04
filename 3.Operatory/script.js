ae8427762d1fb4b20a7845857c5c55d43144f026
DonelK <72340683+DonelK@users.noreply.github.com>
2020-10-04 13:39:07 +0200

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,64 @@
+let isEmpty = false;
+let isGold = true;
+
+console.log(isEmpty == isGold);
+
+
+
+let firstVariable = 4;
+let secondVariable = 3;
+let moduloResult = 0;
+
+moduloResult = firstVariable % secondVariable;
+
+console.log(moduloResult);
+
+
+
+var someNumber = 425;
+var someString = "425";
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+
+var someNumber = "4";
+var someString = "425";
+let stringsResult = "";
+stringsResult = someNumber + someString;
+console.log(stringsResult);
+
+
+
+
+let counter = 30;
+
+console.log(counter);
+
+counter++;
+
+console.log(counter);
+
+counter--;
+
+console.log(counter);
+
+
+
+
+
+let variableOne=30;
+let variableTwo=20;
+let result=null;
+result=variableOne>variableTwo;
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
